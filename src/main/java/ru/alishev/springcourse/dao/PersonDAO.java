7bc5dcfa4e144feca2ccfeb5c86caeb527b89ed2
yrkash <yrkash@gmail.com>
2023-01-05 13:20:02 +0300

    Realized methods with hibernate: show, save, update, delete.

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package ru.alishev.springcourse.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alishev.springcourse.models.Person;
@@ -27,24 +28,36 @@ public class PersonDAO {
         Session session = sessionFactory.getCurrentSession();
         List<Person> people = session.createQuery("select p from Person p", Person.class)
                 .getResultList();
-
-
         return people;
     }
 
+    @Transactional
     public Person show(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.createQuery("select p from Person p where p.id=:id", Person.class).setParameter("id", id)
+                .getSingleResult();
+        return person;
     }
 
+    @Transactional
     public void save(Person person) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        person.setName(updatedPerson.getName());
+        person.setAge(updatedPerson.getAge());
+        session.save(person);
     }
 
+    @Transactional
     public void delete(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        session.remove(person);
     }
 }
